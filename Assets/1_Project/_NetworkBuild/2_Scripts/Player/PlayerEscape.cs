e364773ed2facec70f794a752c92176f1a872ecf
TheQwerty890 <79304415+TheQwerty890@users.noreply.github.com>
2025-04-15 16:03:38 +1000

    End Game functionality working. but
    Issue with swapping back to lobby, players carry over

---
+++ Assets/1_Project/_NetworkBuild/2_Scripts/Player/PlayerEscape.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class PlayerEscape : MonoBehaviour
+{
+    private ObjectiveManager objectiveManager;
+    private NetworkSceneManager networkSceneManager;
+
+    private void Start()
+    {
+        objectiveManager = ObjectiveManager.Instance;
+        networkSceneManager = NetworkSceneManager.Instance;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("boat_escape_point") && objectiveManager.CanPlayersEscape())
+        {
+            networkSceneManager.fn_GoToScene("4_Lobby");
+            Debug.Log("EndGame");
+        }
+    }
+}
