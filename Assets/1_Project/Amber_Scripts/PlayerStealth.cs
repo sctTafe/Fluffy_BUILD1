035595b26f2031c28dd5b04c9b8a7a88b61d42a1
August <qutn11138947@gmail.com>
2025-04-25 15:52:20 +1000

    Stealth mechanics prototype

---
+++ Assets/1_Project/Amber_Scripts/PlayerStealth.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+using Unity.Netcode;
+
+public class PlayerStealth : NetworkBehaviour
+{
+	GameObject geometry;	
+	private bool in_bush = false;
+	private float time_in_bush = 0;
+
+    void Start()
+    {
+    	geometry = transform.GetChild(2).gameObject;    
+    }
+
+	void Update()
+	{
+		if(IsOwner)
+			return;
+
+		if(in_bush)
+		{
+			time_in_bush += Time.deltaTime;
+
+			if(time_in_bush > 3.0)
+			{
+				geometry.SetActive(false);
+				Debug.Log("Player Hidden!");
+			}
+		}
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+    	if(IsOwner)
+			return;
+	
+		if(other.CompareTag("hide_trigger"))
+		{
+			in_bush = true;
+			time_in_bush = 0;
+		}
+	}
+
+	void OnTriggerExit(Collider other)
+	{
+    	if(IsOwner)
+			return;
+	
+		if(other.CompareTag("hide_trigger"))
+		{
+			geometry.SetActive(true);
+			in_bush = false;
+			Debug.Log("Player not hidden!");
+		}
+	}
+
+	
+}
