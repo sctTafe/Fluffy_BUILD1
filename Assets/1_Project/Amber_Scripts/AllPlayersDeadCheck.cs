f52be4a5cdfc450c559f205b07dd975cffabff5a
August <qutn11138947@gmail.com>
2025-05-09 17:16:23 +1000

    Checks if all players are dead and if they are the game ends

---
+++ Assets/1_Project/Amber_Scripts/AllPlayersDeadCheck.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class AllPlayersDeadCheck : MonoBehaviour
+{
+	private float check_cooldown = 6;
+
+    void Update()
+    {
+		check_cooldown -= Time.deltaTime;
+		
+		if(check_cooldown <= 0)
+		{
+			check_cooldown = 6;
+
+			GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
+
+			if(players.Length == 0)
+			{
+				MainGameManager.Instance.fn_EndGame(true);
+			}
+		}
+
+		// Debug key to give playtester more time when playing alone
+		if(Input.GetKeyDown(KeyCode.P))
+		{
+			check_cooldown = 99999;
+		}
+        
+    }
+}
