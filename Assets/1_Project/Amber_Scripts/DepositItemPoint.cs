9dfeaef84d969b89202709bc686eb9ed8211f415
August <qutn11138947@gmail.com>
2025-05-17 19:36:32 +1000

    Update DepositItemPoint.cs

--- Assets/1_Project/Amber_Scripts/DepositItemPoint.cs
+++ Assets/1_Project/Amber_Scripts/DepositItemPoint.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 using Unity.Netcode;
 using TMPro;
 using System;
+using System.Collections;
+using FMODUnity;
 
 public class DepositItemPoint : NetworkBehaviour
 {
@@ -13,6 +15,8 @@ public class DepositItemPoint : NetworkBehaviour
 	*
 	* Don't forget to tag the deposit point deposit_point !
 	**/
+	
+	public EventReference deposit_sound;
 
 	public int amount_needed = 3;
 	public string item_needed = "yellow_test_item";
@@ -21,28 +25,36 @@ public class DepositItemPoint : NetworkBehaviour
 	public string objective_UI_tag = "objective_prompt";
 	
 	private ObjectiveManager objective_manager;
-	private TMP_Text objective_prompt;
+	[SerializeField] private TMP_Text objective_prompt;
 
-	public void Start()
+    public void Start()
 	{
-		var GO = GameObject.FindWithTag(objective_UI_tag);
-		if (GO != null)
-		{
-            objective_prompt = GO.GetComponent<TMP_Text>();
-            if (objective_prompt != null)
-            {
-                objective_prompt.text = $"{objective_name} {current_amount.Value} / {amount_needed}";
-            }
+        // Disable Self If Not Owner
+		/**
+        if (!IsOwner)
+        {
+            this.enabled = false;
+            return;
         }
-
-		var GO2 = GameObject.FindWithTag("ObjectiveManager");
-
-        if (GO2 != null)
-		{
-            objective_manager = GO2.GetComponent<ObjectiveManager>();
-        }
-            
+		**/
+		UpdateUI();
 	}
+
+	void Update()
+	{
+		/**
+		if(objective_prompt == null && Time.frameCount % 10 == 0)
+		{
+			objective_prompt = GameObject.FindWithTag(objective_UI_tag).GetComponent<TMP_Text>();
+			
+			if(objective_prompt != null)
+			{
+				UpdateUI();
+			}
+		}
+		**/
+	}
+
     private void OnEnable()
     {
 		current_amount.OnValueChanged += HandleOnValueChange;
@@ -68,7 +80,7 @@ public class DepositItemPoint : NetworkBehaviour
 
 	public void DepositItem()
 	{
-		IncreaseAmountServerRPC();
+        IncreaseAmountServerRPC();
 	}
 
 	[ServerRpc(RequireOwnership = false)]
